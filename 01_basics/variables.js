db7095f65f47fc16f5c99f78bdbff8f2e2bed467
Praduman singh <161158840+mustbeshubh@users.noreply.github.com>
2024-06-04 09:23:32 +0000

    basics of data types

---
+++ 01_basics/variables.js
@@ -0,0 +1,37 @@
+"use strict"; //to use the newer vers of js 
+
+
+
+
+/************************************ DATA TYPES  ***************************************/
+ // alert(3+3) cant use since this is nodejs and not a browser 
+
+// bolean => true/false
+// symbols
+// null
+// undefined
+
+/**********************************************DATA CONVERSUIONS****************************** */
+
+//let accountId = "123abc" ;
+
+
+//let valueinnum = Number(accountId)
+//console.log(valueinnum)
+
+//let isloggedin = "";
+//let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin);
+//1=> true  , 0=> false 
+// "shubh"=> true , "" => false
+
+
+/* ****************************OPERATIONS **************************/
+/*
+let gamecounter = 110;
+++gamecounter;
+console.log(gamecounter)
+*/
+
+
+// typeof val of undefined is null
